102f6af23894ed4af338d3041a8cca2d00bda9a4
locisvv <locisvv@gmail.com>
2013-06-30 17:35:31 +0300

    fix

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ require 'data_mapper'
 #   DataMapper.setup(:default, 'sqlite:db.sqlite3')
 # end
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite:db.sqlite3')
+DataMapper.setup(:default, ENV['DATABASE_URL'])
 
 class User
   include DataMapper::Resource
