9313c8a7201e9dff2ef94d71e457a226b8430ce9
locisvv <locisvv@gmail.com>
2013-11-06 00:47:03 +0200

    added gallery and albums

--- controllers/photo.rb
+++ controllers/photo.rb
@@ -10,9 +10,6 @@ post '/post/upload_photo' do
 	photos = params[:img]
 
 	photos.each do |photo|
-		print "+++++++++" + photo.to_s
-		print "=========" + album.id.to_s
-
 		photo_item = upload_photo(photo[1], "photos")
 		photo_item.update(album_id: album.id)
 	end
@@ -20,9 +17,15 @@ post '/post/upload_photo' do
 	redirect '/'
 end
 
-get '/albums/:id' do
+get '/album/:id' do
 	@album = Album.first(:id => params[:id])
 	@photos = Photo.all(:album_id => params[:id])
 
 	erb :album
+end
+
+get '/all_albums' do
+	@albums = Album.all
+
+	erb :all_albums
 end
